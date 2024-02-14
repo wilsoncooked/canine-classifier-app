bbf77c0ed3357bd668777e849354920a36719e54
Sarah Wilsoncook <sarahlouise125@gmail.com>
2024-02-14 14:59:09 +0100

    add secret.toml and improve navigation

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Canine Classifier 🐶")
-
-image = st.image("https://images.unsplash.com/photo-1558929996-da64ba858215?q=80&w=2946&auto=format",  use_column_width=True)
